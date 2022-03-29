d57c11f40ac17967ff855733d9a31623f874e891
C1Ou <252844982@qq.com>
2022-03-29 10:03:30 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,16 +3,18 @@ function init(){
 
 	function myEventFunction(){
 		var t1 = document.getElementById('entryinput');
+		var t2 = document.getElementById('textoutput');
 
 
 		alert("Guangkun Ou:" + t1.value);
+		t2.innerHTML = t1.value;
 		
 	}
 	var e1 = document.getElementById('entrybutton');
 	e1.addEventListener('click', myEventFunction);
 	
-	var t2 = document.getElementById('textoutput');
-	t2.innerHTML = t1.value;
+	
+	
 	
 
 	
